97bd9b21c031458a73966831e6ea3d41ed0fb666
Tiago Quadros <contato@tiagoquadros.com.br>
2025-02-05 11:03:23 -0300

    Forgot to put some styling in the main route

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,13 @@
-<script setup lang="ts"></script>
-
 <template>
-  <main class="flex flex-col gap-2 justify-center items-center">
-    <h1 class="text-5xl text-white">Aim Lab</h1>
+  <main class="flex flex-col gap-4 justify-center items-center bg-gray-800 text-white h-screen">
+    <h1 class="text-8xl italic">Aim Lab Vue</h1>
     <p>A cheap version to practice some Vue3</p>
     <div class="flex flex-col gap-2 justify-center items-center">
-      <RouterLink to="/game" class="text-blue-500">Start the Game</RouterLink>
-      <RouterLink to="/about" class="text-blue-500">About</RouterLink>
+      <RouterLink
+        to="/game"
+        class="p-2 px-4 rounded bg-green-700 text-white hover:bg-green-600 transition-all duration-300 cursor-pointer"
+        >Start the Game</RouterLink
+      >
     </div>
   </main>
 </template>
